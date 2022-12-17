9881dcd1be84055f504555b6d1532387a48d7586
Simone <sdiricco.91@gmail.com>
2022-12-17 23:17:48 +0100

    add refactoring

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -20,6 +20,10 @@
 </template>
 
 <script lang="ts" setup>
+
+import { computed } from "vue";
+
+/* IONIC COMPONENTS */
 import {
   IonContent,
   IonHeader,
@@ -27,67 +31,28 @@ import {
   IonLoading,
 } from "@ionic/vue";
 
+/* APP COMPONENTS */
 import ResearchResults from "@/components/HomePage/ResearchResults.vue";
 import NoConnection from "@/components/HomePage/NoConnection.vue";
 import NoResults from "@/components/HomePage/NoResults.vue";
 import ResearchHint from "@/components/HomePage/ResearchHint.vue";
 import SearchToolbar from "@/components/HomePage/SearchToolbar.vue";
 
-import { computed } from "vue";
+/* STORE */
 import { useStore } from "@/store/main";
+
+/*********************************************************/
+/* GLOBALS */
+/*********************************************************/
 const store = useStore();
 
 /*********************************************************/
-/* COMPUTED */
+/* LOCAL COMPUTED */
 /*********************************************************/
-
-const retryMessage = computed(() => {
-  console.log(store.httpRequestRetryCount);
-  return `Sto cercando di connettermi al server ma non risonde. Riprovo..`;
-});
-
-const noConnection = computed(() => {
-  return !store.networkStatus.connected;
-});
-
-const researchHint = computed(() => {
-  return store.searchCity === "";
-});
-
-const noResults = computed(() => {
-  return !store.cities.length && store.searchCity !== "";
-});
-
-const areResults = computed(() => {
-  return store.cities.length > 0;
-});
+const retryMessage = computed(() => `Sto cercando di connettermi al server ma non risonde. Riprovo..`);
+const noConnection = computed(() => !store.networkStatus.connected);
+const researchHint = computed(() => store.searchCity === "");
+const noResults = computed(() => !store.cities.length && store.searchCity !== "");
+const areResults = computed(() => store.cities.length > 0);
 
 </script>
-
-<style scoped>
-ion-icon {
-  font-size: 24px;
-}
-
-ion-buttons {
-  margin: auto;
-}
-
-ion-searchbar {
-  --border-radius: 24px;
-  padding-bottom: 0px;
-  padding-top: 0px;
-}
-
-.pt64 {
-  padding-top: 64px;
-}
-
-.pb16 {
-  padding-bottom: 16px;
-}
-
-.pr16 {
-  padding-right: 16px;
-}
-</style>
